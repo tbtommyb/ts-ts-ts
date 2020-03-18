b8056f047bed30ee3292bb77e00313065e54ef02
Tom Johnson <tom@tmjohnson.co.uk>
2020-03-18 17:57:02 +0000

    Make cells selectable
    Use Set to store local state ahead of Redux introduction

---
+++ src/components/Cell.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import cx from "classnames";
+
+interface CellProps {
+  ident: string
+  selected: boolean
+  handleClick(ident: string): any
+}
+
+const Cell: React.SFC<CellProps> = ({ ident, selected, handleClick }) => {
+  const styles = cx("cell", selected && "selected");
+
+  return (
+    <div className={styles} id={ident} key={ident} onClick={() => handleClick(ident)}></div>
+  );
+
+};
+
+export default Cell;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Instruments from "../instruments";
+import Cell from "./Cell";
 
 const numCells = 16;
 
@@ -7,34 +8,36 @@ interface GridProps {
   instruments: Instruments
 }
 
-interface CellProps {
-  instrument: Instruments
-}
 
-const Cells: React.SFC<CellProps> = ({ instrument }) => {
+const Grid: React.SFC<GridProps> = ({ instruments }) => {
   const cellArray = [...Array(numCells).keys()];
-  return (
-    <div className="cells"> {
-      cellArray.map(i => (
-        <div className="cell" id={`cell-${instrument}-${i}`} key={`${instrument}-${i}`} ></div>
-      ))
-    } </div>
-  );
-};
+  const [activeCells, setActiveCells] = React.useState(new Set());
 
-const Grid: React.SFC<GridProps> = ({ instruments }) => (
-  <div>
-    {
-      Object.keys(instruments).map(instrument => {
-        return (
-          <div id={`row-${instrument}`} className="row align-centre" key={`${instrument}`}>
-            <p className="label instrument-label">{instrument}</p>
-            <Cells instrument={instrument as Instruments} />
-          </div>
-        )
-      })
+  const handleClick = (ident: string) => {
+    let newCells = new Set(activeCells);
+    if (newCells.has(ident)) {
+      newCells.delete(ident);
+    } else {
+      newCells.add(ident);
     }
-  </div >
-);
+    setActiveCells(newCells);
+  }
+
+  return (
+    <div>{
+      Object.keys(instruments).map(instrument => (
+        <div id={`row-${instrument}`} className="row align-centre" key={`${instrument}`}>
+          <p className="label instrument-label">{instrument}</p>
+          <div className="cells-row"> {
+            cellArray.map(i => {
+              let ident = `cell-${instrument}-${i}`;
+              return <Cell ident={ident} key={ident} selected={activeCells.has(ident)} handleClick={handleClick} />
+            })
+          }</div>
+        </div>
+      ))
+    }</div >
+  );
+}
 
 export default Grid;
